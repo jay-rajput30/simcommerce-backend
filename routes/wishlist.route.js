9f4b83913214d3fddabe41aa702b5acb2d9212bf
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2021-07-28 10:21:01 +0530

    refactor: updated wishlist route

--- routes/wishlist.route.js
+++ routes/wishlist.route.js
@@ -26,7 +26,7 @@ router.post("/:id", async (req, res) => {
   try {
     const wishlistId = req.params.id;
     const { productId } = req.body;
-    const newWishListItem = await Wishlist.findOne(`${wishlistId}`);
+    const newWishListItem = await Wishlist.findById(`${wishlistId}`);
     // console.log(newWishListItem);
     newWishListItem.products.push(productId);
     await newWishListItem.save();
@@ -36,23 +36,30 @@ router.post("/:id", async (req, res) => {
   }
 });
 
-router.post("/delete/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const wishlistId = req.params.id;
     const { removeProductId } = req.body;
     const wishlistItem = await Wishlist.findById(`${wishlistId}`);
+    const index = wishlistItem.products.findIndex(
+      (productId) => productId === removeProductId
+    );
+    wishlistItem.products.splice(index, 1);
+    await wishlistItem.save();
+    console.log(wishlistItem);
 
-    const updatedWishlistItems = wishlistItem.products.filter((item) => {
-      return item.toString() !== removeProductId;
-      updatedWishlistItems.save();
-      console.log(item);
-    });
-
-    wishlistItem = {
-      ...wishlistItem,
-      products: [...wishlistItem.products, updatedWishlistItems],
-    };
+    // const updatedWishlist = await Wishlist.findById({});
     res.status(200).json({ success: true, wishlistItem });
+    // const updatedWishlistItems = wishlistItem.products.filter((item) => {
+    //   return item.toString() !== removeProductId;
+    //   updatedWishlistItems.save();
+    //   console.log(item);
+    // });
+
+    // wishlistItem = {
+    //   ...wishlistItem,
+    //   products: [...wishlistItem.products, updatedWishlistItems],
+    // };
   } catch (err) {
     res.status(503).json({ success: false, err });
   }
